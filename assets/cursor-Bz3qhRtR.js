3af34c1d548d6d9b1ee4173e8782de1d94b76181
Luigi Rivera <91353@sistemas.frc.utn.edu.ar>
2025-10-19 20:42:33 -0300

    first prototype production build

---
+++ assets/cursor-Bz3qhRtR.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-C_ZgevCW.js";import"./index-BD37C2wJ.js";import"./events-CJlxH2g0.js";import"./index.es-ve89HPCC.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
---
+++ assets/warning-circle-ouK2ZBMU.js
@@ -0,0 +1,12 @@
+import{b as r}from"./core-C_ZgevCW.js";import"./index-BD37C2wJ.js";import"./events-CJlxH2g0.js";import"./index.es-ve89HPCC.js";import"./index-nibyPLVP.js";const n=r`<svg fill="none" viewBox="0 0 20 20">
+  <path
+    fill="currentColor"
+    d="M11 6.67a1 1 0 1 0-2 0v2.66a1 1 0 0 0 2 0V6.67ZM10 14.5a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5Z"
+  />
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M10 1a9 9 0 1 0 0 18 9 9 0 0 0 0-18Zm-7 9a7 7 0 1 1 14 0 7 7 0 0 1-14 0Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{n as warningCircleSvg};
